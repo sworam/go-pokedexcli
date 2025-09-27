3d5e5362041e5575fe5979200747685733cb5abc
Marlo Swora <swora@uni-hildesheim.de>
2025-09-27 14:42:49 +0200

    refactored commands into their separate file

---
+++ commands.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sworam/pokeapi"
+	"os"
+)
+
+func commandExit(c *config) error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp(c *config) error {
+	fmt.Print("Usage:\n\n")
+
+	for _, command := range commandRegistry {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	return nil
+}
+
+func commandMap(c *config) error {
+	location, err := pokeapi.GetLocation(c.next)
+	if err != nil {
+		return err
+	}
+	displayLocation(location)
+	c.next = location.Next
+	c.previous = location.Previous
+	return nil
+}
+
+func commandMapb(c *config) error {
+	if c.previous == "" {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+
+	location, err := pokeapi.GetLocation(c.previous)
+	if err != nil {
+		return err
+	}
+
+	displayLocation(location)
+	c.next = location.Next
+	c.previous = location.Previous
+	return nil
+}
+
+func displayLocation(location pokeapi.Location) {
+	for _, result := range location.Results {
+		fmt.Println(result.Name)
+	}
+}
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"github.com/sworam/pokeapi"
 	"os"
 	"strings"
 )
@@ -74,53 +73,4 @@ func cleanInput(text string) []string {
 		}
 	}
 	return cleanedWords
-}
-
-func commandExit(c *config) error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
-func commandHelp(c *config) error {
-	fmt.Print("Usage:\n\n")
-
-	for _, command := range commandRegistry {
-		fmt.Printf("%s: %s\n", command.name, command.description)
-	}
-	return nil
-}
-
-func commandMap(c *config) error {
-	location, err := pokeapi.GetLocation(c.next)
-	if err != nil {
-		return err
-	}
-	displayLocation(location)
-	c.next = location.Next
-	c.previous = location.Previous
-	return nil
-}
-
-func commandMapb(c *config) error {
-	if c.previous == "" {
-		fmt.Println("you're on the first page")
-		return nil
-	}
-
-	location, err := pokeapi.GetLocation(c.previous)
-	if err != nil {
-		return err
-	}
-
-	displayLocation(location)
-	c.next = location.Next
-	c.previous = location.Previous
-	return nil
-}
-
-func displayLocation(location pokeapi.Location) {
-	for _, result := range location.Results {
-		fmt.Println(result.Name)
-	}
 }
